7d9828a333ce97deb1e42ea5feef632e481baae2
Nikos Bakalis <47317522+NikosBakalis@users.noreply.github.com>
2019-10-27 19:36:15 +0200

    Add files via upload

--- Quad_Tree/QuadTree.py
+++ Quad_Tree/QuadTree.py
@@ -1,20 +1,37 @@
-class Point:
+from collections import namedtuple
+
+nodes = [(7, 2), (6, 9), (5, 7), (4, 5), (9, 6), (1, 9), (9, 7), (5, 2), (3, 1)]
+
+
+class Node(namedtuple('Node', 'x y')):
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 
-class Rectangle:
-    def __contains__(self, point):
-        x, y = point
+class Rectangle(namedtuple('Rectangle', 'x y w h')):
+    def __contains__(self, node):
+        x, y = node
         if not self.x <= x <= self.x + self.w:
             return False
         if not self.y <= y <= self.y + self.h:
             return False
         return True
 
-    def split(self):
-
 
 class QuadTree:
-    def __init__(self, width, height):
+    def __init__(self, maxwidth, maxheight):
+        self.maxwidth = maxwidth
+        self.maxheight = maxheight
+
+    def node_insertion():
+        if not nodes:
+            print("No nodes found")
+            return None
+        print("Some nodes found")
+
+
+QuadTree.node_insertion()
+
+
+
