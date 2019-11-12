ede210725756d464d6ffc9ebe54cbb0a2662ccb4
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2019-11-12 17:54:00 +0200

    Build,Search and Main

---
+++ KD_Tree/mainKdTree.py
@@ -0,0 +1,12 @@
+import medianBuild
+import searchPoint
+
+points = [(7,2), (5,4), (9,6), (4,7), (8,1), (2,3)]
+dimensions = 2
+
+root = medianBuild.kdTreeBuild(points, 0) #Build the tree
+
+if (searchPoint.kdTreeSearch(root, (8, 1))):
+    print("Found")
+else:
+    print("Not Found")
---
+++ KD_Tree/searchPoint.py
@@ -0,0 +1,13 @@
+dimensions = 2
+
+def kdTreeSearch(node, point, depth = 0):
+    if node is None:
+        return False
+    if node.node == point:
+        return True
+
+    axis = depth % dimensions
+    if (point[axis] < node[0][axis]):
+        return kdTreeSearch(node.leftChild, point, depth + 1) #Search left sub tree
+    else:
+        return kdTreeSearch(node.rightChild, point, depth + 1) #Search right sub tree
