6c53d6b16e34056a53f4cd66f909cb136f211f12
Dimitris Kostorrizos <57042407+DimitrisKostorrizos@users.noreply.github.com>
2019-11-12 18:18:49 +0200

    Build,Search,Rebalance and Main

--- KD_Tree/mainKdTree.py
+++ KD_Tree/mainKdTree.py
@@ -1,5 +1,6 @@
 import medianBuild
 import searchPoint
+import rebalance
 
 points = [(7,2), (5,4), (9,6), (4,7), (8,1), (2,3)]
 dimensions = 2
@@ -10,3 +11,5 @@ if (searchPoint.kdTreeSearch(root, (8, 1))):
     print("Found")
 else:
     print("Not Found")
+
+root = rebalance.balanceKdTree(root)
