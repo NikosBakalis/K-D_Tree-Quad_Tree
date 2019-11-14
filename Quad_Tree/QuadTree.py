65fd2a4466fa69f8b82f53a1745300ee20b91129
NikosBakalis <bakalis.nikolas@gmail.com>
2019-11-14 17:27:46 +0200

    This is the first test

--- Quad_Tree/QuadTree.py
+++ Quad_Tree/QuadTree.py
@@ -8,20 +8,16 @@ class QT:
 
 def insert():
     points = [[-1, 1], [1, 1], [-1, -1], [1, -1]]
-    for point in points.length:
+    for point in points:
         print(point)
         if -5 <= point[0] <= 0 and 0 <= point[1] <= 5:
             print("Top Left")
-            points.remove(point)
         if 0 <= point[0] <= 5 and 0 <= point[1] <= 5:
             print("Top Right")
-            points.remove(point)
         if -5 <= point[0] <= 0 and -5 <= point[1] <= 0:
             print("Bottom Left")
-            points.remove(point)
         if 0 <= point[0] <= 5 and -5 <= point[1] <= 0:
             print("Bottom Right")
-            points.remove(point)
     print(points)
 
 
