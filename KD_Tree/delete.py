4d702c9db53e4d7441d502f849699e42df1cb281
Homi <44709877+HomiG@users.noreply.github.com>
2019-11-21 23:39:10 +0200

    Delete if node is leaf

--- KD_Tree/delete.py
+++ KD_Tree/delete.py
@@ -1,1 +1,15 @@
-1
+import searchPoint
+
+
+def delete(root, point):
+    node = searchPoint.kdTreeSearch(root, point)
+    if node['found'] is False:
+        print("Node to be deleted, Not Found")
+    else:
+        if node['node'].leftChild is None and node['node'].rightChild is None:
+            if node['parentNode'].leftChild == node['node']:
+                node['parentNode'].leftChild
+            if node['parentNode'].rightChild == node['node']:
+                node['parentNode'].rightChild = None
+        else:
+            print("Not None")
