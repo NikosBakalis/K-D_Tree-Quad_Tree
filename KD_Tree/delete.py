a7729203b3a0056e3641aa210c7d15264c484ad0
Homi <44709877+HomiG@users.noreply.github.com>
2020-01-20 18:17:25 +0200

    Add files via upload

--- KD_Tree/delete.py
+++ KD_Tree/delete.py
@@ -1,19 +1,22 @@
-import searchPoint
+from KDTree import searchPoint
+from KDTree import findMinimum
+from KDTree.findMinimum import findMin
 
 
-def delete(root, point):
-    node = searchPoint.kdTreeSearch(root, point)
-    if node['found'] is False:
+def deleteNode(root, point):
+    searchedNode = searchPoint.kdTreeSearch(root, point)
+    if searchedNode['found'] is False:
         print("Node to be deleted, Not Found")
     else:
-        if node['node'].leftChild is None and node['node'].rightChild is None:
-            if node['parentNode'].leftChild == node['node']:
-				temp = node['parentNode'].leftChild
-                node['parentNode'].leftChild = None
-				del temp
-            if node['parentNode'].rightChild == node['node']:
-				temp = node['parentNode'].leftChild
-                node['parentNode'].rightChild = None
-				del temp
-        else:
-            print("Not None")
+        if searchedNode['node'].leftChild is None and searchedNode['node'].rightChild is None:
+            if searchedNode['parentNode'].leftChild == searchedNode['node']:
+                searchedNode['parentNode'].leftChild = None
+            if searchedNode['parentNode'].rightChild == searchedNode['node']:
+                searchedNode['parentNode'].rightChild = None
+        elif searchedNode['node'].rightChild is not None:  # If node to be deleted has a right subtree
+            minNode = findMin(searchedNode['node'].rightChild, searchedNode['depth'] % 2)
+
+            # copy the minimum to the root
+            searchedNode['node'].node = minNode.node
+        # Recursively delete the minimum
+            searchedNode['node'].rightChild = deleteNode(searchedNode['node'].rightChild, minNode.node)
