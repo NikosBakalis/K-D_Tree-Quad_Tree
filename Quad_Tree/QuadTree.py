ca23efc58daf0b520c024432f95bcb6f93acd5f5
Nikos Bakalis <47317522+NikosBakalis@users.noreply.github.com>
2020-01-22 22:41:42 +0200

    Points from file added!

--- Quad_Tree/QuadTree.py
+++ Quad_Tree/QuadTree.py
@@ -5,10 +5,19 @@ import insert
 import traverseTree
 import gatherTreeNodes
 import rebalance
+import csv
 
+x_y_values = []
+with open('points', 'r') as f:
+    reader = csv.reader(f, delimiter=' ')
+    for row in reader:
+        x_y_values.append((int(row[0]), int(row[1])))
+print(x_y_values)
+
+points = x_y_values
 
 # points = [(-3, 1), (1, 1), (-1, -5), (1, -1), (1, 2), (2, 2), (0.5, 2.7)]
-points = [(1, 1), (1, -1), (-1, 1), (-1, -1), (2, 3), (4, 1), (-1, -5), (-3, 9), (6, -1)]   # For some reason last node pops an error, exclude it to run!
+points = x_y_values # [(1, 1), (1, -1), (-1, 1), (-1, -1), (2, 3), (4, 1), (-1, -5), (-3, 9)]    # For some reason last node pops an error, exclude it to run!
 dimensions = 2
 print("test")
 
